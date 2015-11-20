995063ed2d562077b5a0b580714874d7a559f1ab
Joshua Pelletier <jgpelletier@gmail.com>
2015-11-19 21:25:01 -0800

    Exercises 13-20.

--- ex13.py
+++ ex13.py
@@ -6,3 +6,6 @@ print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+
+fourth = raw_input('Fourth variable: ')
+print "Your fourth variable is:", fourth
