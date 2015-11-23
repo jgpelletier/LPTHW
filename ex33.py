c88b3a30a4a5b935ace4c8db82cc5f3f1eb522ca
Joshua Pelletier <jgpelletier@gmail.com>
2015-11-22 17:13:09 -0800

    Modify exercise.

--- ex33.py
+++ ex33.py
@@ -1,16 +1,30 @@
-i = 0
-numbers = []
+def for_looper(x, z):
+    all_numbers = range(0, x, z)
+    numbers = []
+    for num in all_numbers:
+        print 'At the top num is %d' % num
+        numbers.append(num)
+        print "Numbers now: ", numbers
+    return numbers
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def while_looper(x, z):
+    i = 0
+    numbers = []
+    while i < x:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + z
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+    return numbers
 
+numbers = while_looper(60,3)
 
 print "The numbers: "
 
-for num in numbers:
-    print num
+def print_num(numbers):
+    for num in numbers:
+        print num
+
+print_num(numbers)
